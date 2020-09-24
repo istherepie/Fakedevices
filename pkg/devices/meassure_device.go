a00508624b2315271c6d03191d0ae85ff3937cc2
Steffen Park <dev@istherepie.com>
2020-09-24 20:19:52 +0200

    Add meassure and switch type devices

---
+++ pkg/devices/meassure_device.go
@@ -0,0 +1,44 @@
+package devices
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+type Meassure struct {
+	Identifier string
+	Maker      string
+	Value      float32
+}
+
+// MeassuringDevice is a device which randomly emits a float value
+func MeassuringDevice(device Device, client MQTT.Client, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	payload := Meassure{
+		Identifier: device.Name,
+		Maker:      device.Maker,
+	}
+
+	for {
+
+		payload.Value = rand.Float32()
+
+		msg, err := json.Marshal(payload)
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		token := client.Publish(device.Topic, 0, false, msg)
+		token.Wait()
+		time.Sleep(time.Duration(device.Interval) * time.Second)
+	}
+}
---
+++ pkg/devices/switch_device.go
@@ -0,0 +1,51 @@
+package devices
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+type Switch struct {
+	Identifier string
+	Maker      string
+	State      string // State should be `on` or `off`
+}
+
+// SwitchDevice is a device which randomly emits an `on/off` state
+func SwitchDevice(device Device, client MQTT.Client, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	payload := Switch{
+		Identifier: device.Name,
+		Maker:      device.Maker,
+	}
+
+	for {
+
+		chance := rand.Intn(10)
+
+		if chance < 5 {
+			payload.State = "on"
+		} else {
+			payload.State = "off"
+		}
+
+		msg, err := json.Marshal(payload)
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		token := client.Publish(device.Topic, 0, false, msg)
+		token.Wait()
+
+		time.Sleep(time.Duration(device.Interval) * time.Second)
+	}
+}
